7ba1b3d9b67762d28e33efb59ea248e04dd7b63b
frank-bcn <sinnigenfrank@gmail.com>
2023-03-15 15:38:49 +0100

    install move character

--- models/character.class.js
+++ models/character.class.js
@@ -1,20 +1,33 @@
 class Character extends MovableObject {
 
-height = 280;
-y = 145;
-
-constructor() {
-    super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
-    this.loadImages([
+    height = 280;
+    y = 145;
+    IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
         'img/2_character_pepe/2_walk/W-23.png',
         'img/2_character_pepe/2_walk/W-24.png',
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png'
-    ]);
+    ];
 
-}
+    currentImage = 0;
+
+    constructor() {
+        super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+        let path = this.IMAGES_WALKING[this.currentImage];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+        },1000);
+    }
+
 
     jump() {
 
