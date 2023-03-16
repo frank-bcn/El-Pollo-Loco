a1da15964bb48114f7dbddd721961eba6d7062f3
frank-bcn <sinnigenfrank@gmail.com>
2023-03-16 12:53:51 +0100

    install camera

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MovableObject {
 
     height = 280;
     y = 145;
+    speed = 10;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -20,16 +21,44 @@ class Character extends MovableObject {
     }
 
     animate() {
+
+        setInterval(() => {
+            if (this.world.keyboard.Right) {
+                this.x += this.speed;
+                this.otherDirection = false;
+            }
+
+            if (this.world.keyboard.Left) {
+                this.x -= this.speed;
+                this.otherDirection = true;
+            }
+            this.world.camera_x = -this.x;
+
+           /* if (this.world.keyboard.Up) {
+                this.x += this.speed;
+            }
+
+            if (this.world.keyboard.Down) {
+                this.x += this.speed;
+            }
+
+            if (this.world.keyboard.Space) {
+                this.x += this.speed;
+            }*/
+        }, 1000 / 60);
+
+
         setInterval(() => {
 
-            if(this.world.keyboard.Right) {
+            if (this.world.keyboard.Right || this.world.keyboard.Left) {
 
-            let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6; =>0, Rest 0
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
+                // Lauf animation
+                let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6; =>0, Rest 0
+                let path = this.IMAGES_WALKING[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
             }
-        }, 100);
+        }, 50);
     }
 
 
--- models/world.class.js
+++ models/world.class.js
@@ -17,7 +17,7 @@ class World {
     canvas;
     ctx;
     keyboard;
-
+    camera_x = 0;
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -36,10 +36,18 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.ctx.translate(this.camera_x, 0);
+
         this.addObjectsToMap(this.backgroundObjects);// fügt die Hintergrundbilder zur Welt.
+
         this.addObjectsToMap(this.clouds);// fügt die Wolken zur Welt. 
+
         this.addToMap(this.character);// fügt den Character zur Welt.
+
         this.addObjectsToMap(this.enemies);// fügt die Hühner zur Welt.
+
+        this.ctx.translate(-this.camera_x, 0);
 
 
 
@@ -60,7 +68,17 @@ class World {
 
     //Eine Methode, die auf das Canavas-Element zeichnet.
     addToMap(mo) {
+        if (mo.otherDirection) {
+            this.ctx.save();// speichert den aktuellen contex.
+            this.ctx.translate(mo.width, 0);//drehen das bild einmal.
+            this.ctx.scale(-1, 1);// hier wird das Bild verschoben um die breite des elements.
+            mo.x = mo.x * -1;// hier spiegeln wir die x coordinate
+        }
         this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height)//ctx.drawImage ist eine Canvas-API-Methode, die ein Bild auf dem Canvas zeichnet. Die Methode erwartet vier Parameter:
+        if (mo.otherDirection) {
+            mo.x = mo.x * -1;
+            this.ctx.restore();
+        }
         //mo.img: das Bildobjekt, das auf dem Canvas gezeichnet werden soll
         // mo.x: die x-Position des Bildes auf dem Canvas
         // mo.y: die y-Position des Bildes auf dem Canvas
