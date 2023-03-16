95c890c327116eae454bed1762484e7a1cfb60c8
frank-bcn <sinnigenfrank@gmail.com>
2023-03-16 13:33:48 +0100

    install level1

---
+++ models/level.class.js
@@ -0,0 +1,12 @@
+class Level {
+    enemies;
+    clouds;
+    backgriundObjects;
+
+    constructor(enemies, clouds, backgroundObjects) {
+        this.enemies = enemies;
+        this.clouds = clouds;
+        this.backgroundObjects = backgroundObjects;
+
+    }
+}
