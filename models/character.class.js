9514e76d0d7967cfb687944a99b425a326d2ab39
frank-bcn <sinnigenfrank@gmail.com>
2023-03-17 18:08:57 +0100

    install

--- models/character.class.js
+++ models/character.class.js
@@ -3,6 +3,7 @@ class Character extends MovableObject {
     height = 280;
     y = 10;
     speed = 10;
+    
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
