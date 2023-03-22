fc7270d69f7b2bc78ae551fde86dd30f85c90495
frank-bcn <sinnigenfrank@gmail.com>
2023-03-22 17:35:35 +0100

    install

--- models/chicken-small.class.js
+++ models/chicken-small.class.js
@@ -1,4 +1,4 @@
-class Chicken_small extends MovableObject  {
+class Chicken_small extends MovableObject {
     y = 375;
     height = 40;
     width = 50;
@@ -9,9 +9,14 @@ class Chicken_small extends MovableObject  {
         'img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
     ];
 
+    IMAGES_DIE = [
+        'img/3_enemies_chicken/chicken_small/2_dead'
+    ];
+
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_DIE);
 
         this.x = 300 + Math.random() * 500; // Zahl zwischen 300 und 700.
         this.speed = 0.15 + Math.random() * 0.25;
@@ -27,6 +32,7 @@ class Chicken_small extends MovableObject  {
 
         setInterval(() => {
             this.playAnimation(this.IMAGES_WALKING);
+            if (this.isDead()) this.loadImage(this.IMMAGE_Die);
         }, 140);
     }
 }
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -10,10 +10,15 @@ class Chicken extends MovableObject {
         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
     ];
 
+    IMAGES_DIE = [
+        'img/3_enemies_chicken/chicken_normal/2_dead'
+    ];
+
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_DIE);
 
         this.x = 200 + Math.random() * 500; // Zahl zwischen 200 und 700.
         this.speed = 0.15 + Math.random() * 0.25;
@@ -29,6 +34,7 @@ class Chicken extends MovableObject {
 
         setInterval(() => {
             this.playAnimation(this.IMAGES_WALKING);
+            if (this.isDead()) this.loadImage(this.IMMAGE_Die);
         }, 150);
     }
 }
