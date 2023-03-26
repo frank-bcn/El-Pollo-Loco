c442e70d46125ba1a4de04a0ea091a20da77d6bc
frank-bcn <sinnigenfrank@gmail.com>
2023-03-26 10:21:07 +0200

    clean

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,12 +4,8 @@ class MovableObject extends DrawableObject {
     speedY = 0;
     acceleration = 2.5;
     lastHit = 0;
-    offset = {
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0
-    };
+    
+    offset = {top: 0, bottom: 0, left: 0, right: 0};
 
 
     applyGravity() {
