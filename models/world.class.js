f92d67c60b2c25ac2c70683065740765dd09dce4
frank-bcn <sinnigenfrank@gmail.com>
2023-03-27 18:38:14 +0200

    cleaning

--- models/world.class.js
+++ models/world.class.js
@@ -43,11 +43,11 @@ class World {
     }
 
     checkCollisionsEnemy() {
-        this.level.enemies.forEach((enemy, index) => { //Schleife die durch das level enemies irretiert
+        this.level.enemies.forEach((enemy, y) => { //Schleife die durch das level enemies irretiert
             if (this.character.isColliding(enemy)) {   // prüft ob der character kontakt hat          
                 if (this.character.speedY < 0 && this.character.isAboveGround()) { //überprüft ob der character in der luft ist oder ob er sich auf den boden befindet
-                    this.level.enemies[index].hit(5); // gibt den gegner 10 schaden 
-                } else if (!this.level.enemies[index].isDead()) {// ab hier bekommt der character 5 schaden 
+                    this.level.enemies[y].hit(5); // gibt den gegner 10 schaden 
+                } else if (!this.level.enemies[y].isDead()) {// ab hier bekommt der character 5 schaden 
                     this.character.hit(5);
                     this.statusBarHealth.setPercentage(this.character.hp)
                 }
@@ -57,9 +57,9 @@ class World {
 
     checkCollisionsThrowable() {
         this.throwableObject.forEach((bottle, i) => {// prüft ob Flaschen vorhanden sind.
-            this.level.enemies.forEach((enemy, index) => {//püft ob es ein Kontact zwischen Flasche und Gegner gibt
+            this.level.enemies.forEach((enemy, y) => {//püft ob es ein Kontact zwischen Flasche und Gegner gibt
                 if (bottle.isColliding(enemy)) { // kontakt Gegener
-                    this.level.enemies[index].hit(5);// fügt ein Schaden von 5
+                    this.level.enemies[y].hit(5);// fügt ein Schaden von 5
                     this.throwableObject[i].hit(5);
                 }
             });
