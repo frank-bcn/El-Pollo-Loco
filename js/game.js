d64b41f2b4c0649d8f90c8e2c28b33b987e6eb79
frank-bcn <sinnigenfrank@gmail.com>
2023-04-12 16:54:07 +0200

    install

--- js/game.js
+++ js/game.js
@@ -11,45 +11,45 @@ function init() {
 
 
 window.addEventListener("keydown", (event) => {
-    if(event.keyCode == 39) {
+    if (event.keyCode == 39) {
         keyboard.Right = true;
     }
 
-    if(event.keyCode == 37) {
+    if (event.keyCode == 37) {
         keyboard.Left = true;
     }
 
-    if(event.keyCode == 38) {
+    if (event.keyCode == 38) {
         keyboard.Up = true;
     }
 
-    if(event.keyCode == 40) {
+    if (event.keyCode == 40) {
         keyboard.Down = true;
     }
 
-    if(event.keyCode == 32) {
+    if (event.keyCode == 32) {
         keyboard.Space = true;
     }
 });
 
 window.addEventListener("keyup", (event) => {
-    if(event.keyCode == 39) {
+    if (event.keyCode == 39) {
         keyboard.Right = false;
     }
 
-    if(event.keyCode == 37) {
+    if (event.keyCode == 37) {
         keyboard.Left = false;
     }
 
-    if(event.keyCode == 38) {
+    if (event.keyCode == 38) {
         keyboard.Up = false;
     }
 
-    if(event.keyCode == 40) {
+    if (event.keyCode == 40) {
         keyboard.Down = false;
     }
 
-    if(event.keyCode == 32) {
+    if (event.keyCode == 32) {
         keyboard.Space = false;
     }
 });
@@ -64,24 +64,44 @@ function startGameNow() {
     canvas = document.getElementById("canvas");
     world = new World(canvas, keyboard);
     gameOver();
-  }
+}
 
-  function fullscreen() {
+function fullscreen() {
     document.getElementById('canvas').style = "width:100%;height:100vh";
     fullscreenIconExchange();
-  }
+}
 
-  function closeFullscreen() {
+function closeFullscreen() {
     document.getElementById('canvas').style = "width:720px;height:480px";
     closeFullscreenIconExchange();
-  }
+}
 
-  function fullscreenIconExchange() {
-    document.getElementById('fullscreen-icon').classList.add('d-none');
-    document.getElementById('nofullscreen-icon').classList.remove('d-none');
+function fullscreenIconExchange() {
+    document.getElementById('fullscreen').classList.add('d-none');
+    document.getElementById('nofullscreen').classList.remove('d-none');
 }
 
 function closeFullscreenIconExchange() {
-    document.getElementById('fullscreen-icon').classList.remove('d-none');
-    document.getElementById('nofullscreen-icon').classList.add('d-none');
+    document.getElementById('fullscreen').classList.remove('d-none');
+    document.getElementById('nofullscreen').classList.add('d-none');
+}
+
+function sound() {
+   
+    muteExchange();
+}
+
+function soundMute() {
+   
+    soundExchange();
+}
+
+function soundExchange() {
+    document.getElementById('mute').classList.add('d-none');
+    document.getElementById('speaker').classList.remove('d-none');
+}
+
+function muteExchange() {
+    document.getElementById('speaker').classList.add('d-none');
+    document.getElementById('mute').classList.remove('d-none');
 }
